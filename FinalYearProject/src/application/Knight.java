916b0c51da68749edfa55cd1b786058b72029f8b
Alex Mason <Alexander.Mason.2017@live.rhul.ac.uk>
2019-12-02 03:11:05 +0000

    Added 'setPos' Method in Knight Class
    Added a 'setPos' method which gets called from the 'moveKnight' method once validation (for what counts as a legal move) has been completed. The 'setPos' method sets the position of the Knight piece on the game board.

--- FinalYearProject/src/application/Knight.java
+++ FinalYearProject/src/application/Knight.java
@@ -1,8 +1,12 @@
 package application;
 
+import application.exceptions.InvalidPieceException;
+import application.exceptions.InvalidPlayerException;
+
 public class Knight {
 
   public Board board = new Board();
+  private Piece piece = new Piece();
   
   /**
    * This method is responsible for validating and then moving a Knight
@@ -11,8 +15,11 @@ public class Knight {
    * @param player Takes the current player (Either "White" or "Black").
    * @param selectedKnight Takes the selected Knight piece.
    * @param toTile The destination tile that the Knight intends to move to.
+   * @throws InvalidPlayerException If the player is not "White" or "Black".
+   * @throws InvalidPieceException If an invalid piece is passed to the method.
    */
-  public void moveKnight(String player, String selectedKnight, String toTile) {
+  public void moveKnight(String player, String selectedKnight, String toTile) 
+      throws InvalidPieceException, InvalidPlayerException {
     
     String fromTile = board.map.getTile(selectedKnight);
     
@@ -24,34 +31,42 @@ public class Knight {
     // MOVING UP 2 SPACES AND 1 SPACE LEFT
     if ((toRow == fromRow + 2) && (toColumn == fromColumn - 1)) {
       System.out.println("----- Moving 2 Spaces Up and 1 Space Left -----");
+      setPos(player, selectedKnight, toTile);
     
     // MOVING UP 2 SPACES AND 1 SPACE RIGHT
     } else if ((toRow == fromRow + 2) && (toColumn == fromColumn + 1)) {
       System.out.println("----- Moving 2 Spaces Up and 1 Space Right -----");
+      setPos(player, selectedKnight, toTile);
     
     // MOVING RIGHT 2 SPACES AND 1 SPACE UP
     } else if ((toColumn == fromColumn + 2) && (toRow == fromRow + 1)) {
       System.out.println("----- Moving 2 Spaces Right and 1 Space Up -----");
+      setPos(player, selectedKnight, toTile);
     
     // MOVING RIGHT 2 SPACES AND 1 SPACE DOWN
     } else if ((toColumn == fromColumn + 2) && (toRow == fromRow - 1)) {
       System.out.println("----- Moving 2 Spaces Right and 1 Space Down -----");
+      setPos(player, selectedKnight, toTile);
       
     // MOVING DOWN 2 SPACES AND 1 SPACE LEFT
     } else if ((toRow == fromRow - 2) && (toColumn == fromColumn - 1)) {
       System.out.println("----- Moving 2 Spaces Down and 1 Space Left -----");
+      setPos(player, selectedKnight, toTile);
     
     // MOVING DOWN 2 SPACES AND 1 SPACE RIGHT
     } else if ((toRow == fromRow - 2) && (toColumn == fromColumn + 1)) {
       System.out.println("----- Moving 2 Spaces Down and 1 Space Right -----");
+      setPos(player, selectedKnight, toTile);
     
     // MOVING LEFT 2 SPACES AND 1 SPACE UP
     } else if ((toColumn == fromColumn - 2) && (toRow == fromRow + 1)) {
       System.out.println("----- Moving 2 Spaces Left and 1 Space Up -----");
+      setPos(player, selectedKnight, toTile);
     
     // MOVING LEFT 2 SPACES AND 1 SPACE DOWN
     } else if ((toColumn == fromColumn - 2) && (toRow == fromRow - 1)) {
       System.out.println("----- Moving 2 Spaces Left and 1 Space Down -----");
+      setPos(player, selectedKnight, toTile);
     
     /* If we have none of the above combinations which are valid moves for a Knight, print "Illegal
      * Move". */
@@ -60,5 +75,37 @@ public class Knight {
     }
   }
   
-  
+  /**
+   * This method is called from the moveKnight method above. Its purpose is to minimise redundant/
+   * duplicate code by setting the position of the Knight's new tile in here.
+   * 
+   * @param player Takes the current player (Either "White" or "Black").
+   * @param selectedKnight Takes the selected Knight piece.
+   * @param toTile Takes the destination tile the player intends the Knight to move to.
+   */
+  public void setPos(String player, String selectedKnight, String toTile) 
+      throws InvalidPieceException, InvalidPlayerException {
+    // If the destination tile is empty
+    if (board.map.getPieceOrOccupation("tileOccupation", toTile) == "Empty") {
+      board.map.setValue("piecePos", toTile, selectedKnight);
+    
+    // If the destination tile is occupied with an opponent player's piece
+    } else if (board.map.getPieceOrOccupation("tileOccupation", toTile) == "Occupied"
+        && piece.isOpponentPiece(player, board.map.getPieceOrOccupation("piecePos", toTile)) 
+        == true) {
+      board.map.capturePiece(board.map.getPieceOrOccupation("piecePos", toTile));
+      board.map.setValue("piecePos", toTile, selectedKnight);
+    
+    // If the destination tile is occupied with player's own piece
+    } else if (board.map.getPieceOrOccupation("tileOccupation", toTile) == "Occupied"
+         && piece.isOpponentPiece(player, board.map.getPieceOrOccupation("piecePos", toTile)) 
+         == false) {
+      System.out.println("Illegal Move. You cannot move your Knight in a tile containing "
+          + "your own piece");
+      
+    // Else, do not set the position of Knight and display message.
+    } else {
+      System.out.println("Invalid Move");
+    }
+  }
 }
